c91f234438de35d6e1ec928952a5292c3e1e627a
Ayesha Ali <ayeshaalibsmath@gmail.com>
2024-09-15 23:22:33 +0500

    Update project2.py

--- project2.py
+++ project2.py
@@ -4,4 +4,10 @@ print(type(age))
 print('i am "here" yra')
 result=2+2
 print(result)
+#spacing does not affect the code's execution.
+result = 3+2*5
+print(result) 
+result = 3 + 2 * 5
+print(result)  
+
 
