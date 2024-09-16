028d373b9f5f5bee3da08b43ceb60172d1bb3f45
Ayesha Ali <ayeshaalibsmath@gmail.com>
2024-09-16 10:26:22 +0500

    Update project3.py

--- project3.py
+++ project3.py
@@ -1,2 +1,9 @@
-print("hi)"
+print("hi")
+name=input("what's your name?")
+print("it's nice to meet you,",name)
+answer=input("are you enojying as a my guider?")
+if answer=="yes":
+    print("that's good to hear")
+else:
+    print("oh no!that makes me sad")
       
