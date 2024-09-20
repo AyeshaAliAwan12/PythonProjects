de82f3f03459d9c728c3d68251ab1b5ac5d00259
Ayesha Ali <ayeshaalibsmath@gmail.com>
2024-09-20 18:24:46 +0500

    Create project5.py

---
+++ project5.py
@@ -0,0 +1,28 @@
+#check if the number is even or odd
+number=int(input("enter the number"))
+if number % 2== 0:
+    print("the  number is even")
+else:
+    print("number is odd")
+#check if the number is positive ,negative ,or zero
+Number=int(input("enter the number"))
+if Number >0:
+    print("the number is positive")
+elif Number ==0:
+    print("the number is zero")
+else:
+    print("the number is negative")
+  #simple conversation
+  print("salam")
+name=input("how are you?")
+if name=="fine":
+    print("that's great")
+else:
+    print("oh hoo")
+question=input("where you want to go")
+print("i want to go",question)
+if  question=="janah":
+    print("if you want to go janah you have to obey Allah")
+else:
+    print("do what you want")
+  
