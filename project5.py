150ec06f46e5bc51abb1c2eb9ff0f159575ce07e
Ayesha Ali <ayeshaalibsmath@gmail.com>
2024-09-20 19:27:32 +0500

    Update project5.py

--- project5.py
+++ project5.py
@@ -25,4 +25,13 @@ if  question=="janah":
     print("if you want to go janah you have to obey Allah")
 else:
     print("do what you want")
+    #check which number is larger
+num1=int(input("enter the first number: "))
+num2=int(input("enter the second number: "))
+if num1> num2:
+    print(f"{num1}is greater than {num2}")
+elif num1<num2:
+    print(f"{num2}is greater than {num1}")
+else:
+    print("both are equal")
   
