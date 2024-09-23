df8dc0ab30d0c2ffb040ce2d7ace0a9895ddd198
Ayesha Ali <ayeshaalibsmath@gmail.com>
2024-09-23 20:02:42 +0500

    Create project7.py

---
+++ project7.py
@@ -0,0 +1,15 @@
+#making list in python
+cities=[
+    'khewra',
+    'lahore',
+    'rawalpindi'
+]
+print(cities[1])
+#Making dictionary
+activities={
+    'Morning':'overthinking',
+    'After Noon':'cooking,overthinking about Future',
+    'evening':'depressed',
+    'night':'upset mind'
+}
+print(activities['Morning'])
